d86017f369c4968f2388e2959a4817ee866429ce
Alios <80349968+Alios-5@users.noreply.github.com>
2022-10-10 15:55:30 +0300

    Sound bank manager script + organized different banks

---
+++ Assets/Scripts/Audio/F_Banks.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class F_Banks : MonoBehaviour
+{
+    public string LoadBankAwake;
+    public string LoadBankAwakeStrings;
+
+    public string UnloadBankDestroy;
+    public string UnloadBankDestroyStrings;
+
+    private void Awake()
+    {
+        FMODUnity.RuntimeManager.LoadBank(LoadBankAwake);
+        FMODUnity.RuntimeManager.LoadBank(LoadBankAwakeStrings);
+    }
+
+    private void OnDestroy()
+    {
+        FMODUnity.RuntimeManager.UnloadBank(UnloadBankDestroy);
+        FMODUnity.RuntimeManager.UnloadBank(UnloadBankDestroyStrings);
+    }
+
+}
