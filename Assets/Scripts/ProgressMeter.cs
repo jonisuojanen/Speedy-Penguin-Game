02d0050d992f78b120efb2dd7bd3acc913a5c8eb
Risto Haila <grest.hyber@gmail.com>
2022-10-11 19:58:52 +0300

    Added simple progressbar.

---
+++ Assets/Scripts/ProgressMeter.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressMeter : MonoBehaviour
+{
+    [SerializeField] private Transform m_GoalTransform;
+    [SerializeField] private Transform m_TargetTransform;
+    [SerializeField] private RectTransform m_ProgressImageTransform;
+    
+    [HideInInspector] public float m_Progress;
+    private float m_TotalDistance;
+
+
+    private void Start()
+    {
+        m_TotalDistance = Vector3.Distance(m_GoalTransform.position, m_TargetTransform.position);
+    }
+
+    private void FixedUpdate()
+    {
+        float dst = Vector3.Distance(m_GoalTransform.position, m_TargetTransform.position);
+
+        m_Progress = 1-((m_TotalDistance - dst) / m_TotalDistance);
+        m_ProgressImageTransform.localScale = new Vector3(m_Progress, 1, 1);
+
+    }
+}
