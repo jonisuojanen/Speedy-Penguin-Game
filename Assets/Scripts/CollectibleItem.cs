1634894b51cf3ad8318a340fa5f7154746a680fa
Risto Haila <grest.hyber@gmail.com>
2022-11-14 18:36:48 +0200

    Collectibles look at player.

--- Assets/Scripts/CollectibleItem.cs
+++ Assets/Scripts/CollectibleItem.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class CollectibleItem : MonoBehaviour
 {
+    private Transform m_player;
     [SerializeField] private int m_amount;
     [SerializeField] private float m_rotateSpeed;
-    private float m_rotationY;
+    //private float m_rotationY;
+
+    private void Start()
+    {
+        m_player = GameManager.instance.playerController.transform;
+    }
 
     void Update()
     {
-        m_rotationY = m_rotationY + m_rotateSpeed * Time.deltaTime;
-        if (m_rotationY > 360f) m_rotationY -= 360f;
-        transform.rotation = Quaternion.Euler(transform.rotation.x, m_rotationY, transform.rotation.z);
+        transform.LookAt(m_player);
+        //m_rotationY = m_rotationY + m_rotateSpeed * Time.deltaTime;
+        //if (m_rotationY > 360f) m_rotationY -= 360f;
+        //transform.rotation = Quaternion.Euler(transform.rotation.x, m_rotationY, transform.rotation.z);
     }
 
     private void OnTriggerEnter(Collider other)
